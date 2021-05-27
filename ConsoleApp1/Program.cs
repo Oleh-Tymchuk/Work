4944420a1dadc151b0224eb1e22e325d4eaf4bc6
OLeGaTor-RPG <62431055+OLeGaTor-RPG@users.noreply.github.com>
2021-05-27 14:21:19 +0300

    Added BD

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,83 +2,105 @@
 using System.Collections.Generic;
 using System.Collections;
 using MySql.Data.MySqlClient;
-using Microsoft.SqlServer.Server;
+using System.Linq;
 using Microsoft.SqlServer;
+using System.Data.SqlClient;
 
 namespace WorkTest
 {
-    public class Order : IComparable<Order>, IComparer<Order>
+    class Program
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Details { get; set; }
-        public string City { get; set; }
-        public string Address { get; set; }
-        public DateTime Fulfillment { get; set; }
-        public decimal Price { get; set; }
-        public string PaymentMethod { get; set; }
-        //public Specialist Specialist { get; set; }
-        //public Client Client { get; set; }
-
-        public int Compare(Order ord1, Order ord2)
+        /// <summary>
+        /// Sort Data from SQL by following scope
+        /// </summary>
+        /// <param name="SqlPath"> Must be like this for "MY":"server = ; SSL mode = ; port = ; user = root; database = ; password = "</param>
+        /// <param name="Request"> Your SQL Request. Example: "SELECT Price FROM client" </param>
+        /// <param name="Choose"> Choose Betwenn MS SQL and MySQL. Example: Enter MS for MS SQL and MY for MySQL </param>
+        /// <param name="scope"> Scope for sort</param>
+        public static void SortSqlData(string Choose, string SqlPath, string Request, (int, int) scope)
         {
-            if (ord1.Price > ord2.Price)
+            if (Choose == "MY")
             {
-                return 1;
+                List<string> array = new List<string>();
+
+                string AddSql = SqlPath;
+
+                using (MySqlConnection Connection = new MySqlConnection(AddSql))
+                {
+                    Connection.Open();
+
+                    MySqlCommand command = new MySqlCommand(Request, Connection);
+
+                    MySqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        array.Add(reader[0].ToString());
+
+                    }
+                    reader.Close();
+
+                    Connection.Close();
+                }
+                var result = ConvertAndSort(array, scope);
+                ShowResult(result);
             }
-            else if (ord1.Price < ord2.Price)
+            else if (Choose == "MS")
             {
-                return -1;
+                List<string> array = new List<string>();
+
+                string AddSql = SqlPath;
+                using (SqlConnection Connection = new SqlConnection(SqlPath))
+                {
+                    Connection.Open();
+                    Connection.Close();
+                }
             }
             else
             {
-                return 0;
-            }
-        }
-
-        public int CompareTo(Order ord)
-        {
-            return Price.CompareTo(ord.Price);
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<string> array = new List<string>();
-
-            string AddSQl = "server=localhost; SSL mode = None; port=3306; user=root; database=client; password=0000";
-
-            using (MySqlConnection Connection = new MySqlConnection(AddSQl))
-            {
-                Connection.Open();
-
-                string request = "SELECT Price FROM client WHERE Price > 200 and PaymentMethod = 'VISA'";
-
-                MySqlCommand command = new MySqlCommand(request, Connection);
-
-                MySqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    array.Add(reader[0].ToString());
-
-                }
-                reader.Close();
-
-                Connection.Close();
+                throw new Exception("Didn't choose a right SQL");
             }
 
+        }
+        /// <summary>
+        /// Convert string Data from SQL to Int array and sort it 
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="scope"></param>
+        /// <returns></returns>
+        public static int[] ConvertAndSort(List<string> array, (int, int) scope)
+        {
             var array1 = Array.ConvertAll(array.ToArray(), t => Int32.Parse(t));
 
-            Array.Sort(array1);
+            var count = array1.Where(x => x >= scope.Item1 && x <= scope.Item2);
 
-            foreach (int i in array1)
+            var finalarray = count.ToArray();
+
+            Array.Sort(finalarray);
+
+            return finalarray;
+        }
+        /// <summary>
+        /// Unnecessary method for getting result
+        /// </summary>
+        /// <param name="array"> Data to show </param>
+        public static void ShowResult(int[] array)
+        {
+            foreach (int i in array)
             {
                 Console.WriteLine(i);
             }
-            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            SortSqlData("MY", "server = localhost; SSL mode = none; port = 3306; user = root; database = client; password = 0000", "SELECT Price FROM client", (200, 1000));
+            string AddSql = "Data Source = database-1.cnfs5af4detk.us-east-2.rds.amazonaws.com; Integrated Security = true; Uid = admin; Password = Development-2021; Initial catalog = FTR";
+            using (SqlConnection Connection = new SqlConnection(AddSql))
+            {
+                Connection.Open();
+                Console.WriteLine("Good");
+                Connection.Close();
+            }
         }
     }
 }
